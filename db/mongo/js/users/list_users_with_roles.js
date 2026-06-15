ee722d7abf93ae7f652f17cda549547c5a04e844
Henrique Soares da Silva <henriquesilva@fineasytech.com>
2026-06-15 11:34:25 -0300

    Atualizado list_users.js mongodb

--- db/mongo/js/users/list_users_with_roles.js
+++ db/mongo/js/users/list_users_with_roles.js
@@ -8,3 +8,7 @@ db.system.users.find({}, { user: 1, roles: 1, _id: 0 })
 
 //Buscar por nome
 db.getUser("db_app_admin", { showAuthenticationRestrictions: true })
+
+mongosh --host 10.100.106.86 --port 27017 \
+-u admin -p 'yhz2mtk0vcx7YAG1mbd' --authenticationDatabase admin \
+--eval "config.set('displayBatchSize', 100); db.getSiblingDB('admin').system.users.find({}, {user:1, roles:1, _id:0}).pretty()"
