130ba986ee6914e70e024ab77931713f2c05a3cb
Henrique Soares da Silva <henriquesilva@fineasytech.com>
2026-07-14 15:37:22 -0300

    user privs oracle

--- db/mongo/js/users/list_users.js
+++ db/mongo/js/users/list_users.js
@@ -5,3 +5,5 @@ db.getUsers().users.map(u => u.user)
 
 // Método 2 (coleção interna)
 db.system.users.find({}, { user: 1, _id: 0 })
+
+db.system.users.find({ user: "lucas_cataldo" }, { user: 1, db: 1, roles: 1 })
